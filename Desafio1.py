72b5300144d131ba85e3f972e98c86f0f039b9ae
João Vitor Melo <143234094+JoaoVitorMeloDev@users.noreply.github.com>
2026-04-14 15:07:37 -0300

    Fix syntax errors in Desafio1.py
    Fixed syntax errors by adding missing colons in function definitions.

--- Desafio1.py
+++ Desafio1.py
@@ -1,4 +1,4 @@
-## O Erro está na falnta de : dois pontos no final da 3 linha [def somar(a, b)]
+## O Erro está na falta de : dois pontos no final da 3 linha [def somar(a, b)]
 
 def somar(a, b)
     return a + b
